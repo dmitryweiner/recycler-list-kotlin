2887698dc444706d60527ff4dfa85fd67e780433
Dmitry Weiner <dmitry.weiner@gmail.com>
2022-10-04 13:41:29 +0700

    "Remove" button

--- app/src/main/java/com/example/recycler_list/MainActivity.kt
+++ app/src/main/java/com/example/recycler_list/MainActivity.kt
@@ -2,17 +2,25 @@ package com.example.recycler_list
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val list = mutableListOf<String>()
+    private lateinit var adapter: RecyclerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val list = mutableListOf<String>()
-        val adapter = RecyclerAdapter(list)
+        adapter = RecyclerAdapter(list) {
+            list.removeAt(it)
+            adapter.notifyItemRemoved(it)
+        }
+
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
--- app/src/main/java/com/example/recycler_list/RecyclerAdapter.kt
+++ app/src/main/java/com/example/recycler_list/RecyclerAdapter.kt
@@ -6,7 +6,10 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class RecyclerAdapter(private val list: List<String>): RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+class RecyclerAdapter(
+    private val list: List<String>,
+    private val onItemClick: (id: Int) -> Unit
+) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val itemView = LayoutInflater.from(parent.context)
             .inflate(R.layout.list_item, parent, false)
@@ -15,13 +18,17 @@ class RecyclerAdapter(private val list: List<String>): RecyclerView.Adapter<Recy
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.textView.text = list[position]
+        holder.button.setOnClickListener {
+            onItemClick(holder.adapterPosition)
+        }
     }
 
     override fun getItemCount(): Int {
         return list.size
     }
 
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val textView = itemView.findViewById<TextView>(R.id.textView)
+        val button = itemView.findViewById<TextView>(R.id.button)
     }
 }
